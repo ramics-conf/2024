16defb8ccbe21176c3af913fdf1b203ba5a6672d
Uli Fahrenberg <ulrich.fahrenberg@inria.fr>
2024-05-17 18:08:27 +0200

    add cfsc

--- send-emails.sh
+++ send-emails.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-ADDRS=emails-test.csv
-#ADDRS=ramics-emails.csv
+#ADDRS=emails-test.csv
+ADDRS=private/ramics-emails.csv
 #ADDRS=/tmp/emails-1.csv
 
 FROM=fahrenberg@gmail.com
 
-EMAILCONT=cfp2.txt
+EMAILCONT=cfp3.txt
 
 #SUBJECT='CfP: Relational and Algebraic Methods in Computer Science, RAMiCS 2024, Prague'
-SUBJECT='2nd CfP & Deadline Extension: RAMiCS 2024'
+SUBJECT='3rd CfP & Deadline Extension: RAMiCS 2024'
 
 #TMPDIR=$(mktemp -d)
 TMPDIR=/tmp
@@ -28,7 +28,8 @@ while read -r LINE; do
     echo "To: $EMAIL" >> $TMPDIR/email.txt
     echo "Subject: $SUBJECT" >> $TMPDIR/email.txt
     echo "" >> $TMPDIR/email.txt
-    #echo "Dear $FIRST," >> $TMPDIR/email.txt
+    echo "Dear $FIRST $LAST," >> $TMPDIR/email.txt
+    echo "" >> $TMPDIR/email.txt
     cat $EMAILCONT >> $TMPDIR/email.txt
     cat $TMPDIR/email.txt | ssmtp $EMAIL
     sleep 1 #otherwise, too fast for gmail :)
